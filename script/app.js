1753550ff5c6030828c25764e47c399c24b44320
MdMostakimBillah <mostakimbillahn609@gmail.com>
2025-08-05 16:18:01 +0600

    navigation click functionality data adding in experience section

--- script/app.js
+++ script/app.js
@@ -1,3 +1,8 @@
-function NavigationActive() {
-    let 
-}
+let navItems = document.querySelectorAll(".NavItems");
+
+navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        navItems.forEach(nav => nav.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
