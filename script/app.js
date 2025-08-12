0c998016e7b0c503aef782fac37c00c98b1b865e
MdMostakimBillah <mostakimbillahn609@gmail.com>
2025-08-12 19:21:41 +0600

    Simple change selection tracking

--- script/app.js
+++ script/app.js
@@ -1,8 +1,33 @@
-let navItems = document.querySelectorAll(".NavItems");
-
-navItems.forEach((item) => {
-    item.addEventListener('click', () => {
-        navItems.forEach(nav => nav.classList.remove("active"));
-        item.classList.add("active");
+function navigation () {
+    let navItems = document.querySelectorAll(".NavItems");
+    navItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            navItems.forEach(nav => nav.classList.remove("active"));
+            item.classList.add("active");
+        })
     })
-})
+}
+navigation ();
+
+document.addEventListener('mouseup', (e) => {
+    const selection = window.getSelection();
+    const popup = document.getElementById("profile");
+
+    if(selection.toString().length > 0){
+        const selectedText = selection.toString().trim();
+
+        if (selectedText.toLowerCase() === "mostakim billah" || selectedText.toLowerCase() === "mohammad mostakim billah") {
+            popup.classList.add("showPopup"); 
+        }
+
+    }else{
+        popup.classList.remove("showPopup");
+    }
+});
+
+document.addEventListener("mousedown", function (e) {
+  const popup = document.getElementById("profile");
+  if (!popup.contains(e.target)) {
+    popup.classList.remove("showPopup");
+  }
+});
